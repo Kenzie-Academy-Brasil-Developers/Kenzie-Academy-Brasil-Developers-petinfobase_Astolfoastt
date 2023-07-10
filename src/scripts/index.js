1baf9fac7a32a8a4b9027a352531869c587cad63
Astolfo Daniel <astolfo_007@icloud.com>
2023-07-09 22:17:37 -0300

    realizando entrega

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,39 @@
 /* Desenvolva seu código aqui */
-import { login } from "./requests.js"
+import { login } from "./requests.js";
+import { toasts } from "./toasts.js";
+const red = "#df1545";
 
-function handleLogin () {
-    const inputs = document.querySelectorAll(".containerLogin__input")
-    const button = document.querySelector(".confirm__button")
-    let loginBody = {}
-    let count = 0
+function handleLogin() {
+  const inputs = document.querySelectorAll(".containerLogin__input");
+  const button = document.querySelector(".confirm__button");
+  let loginBody = {};
+  let count = 0;
 
-    button.addEventListener("click", (event) => {
-        event.preventDefault()
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-        inputs.forEach(input => {
-            if(input.value.trim() === ""){
-                count++
-            }
-            loginBody[input.name] = input.value
-        })
-        if(count != 0){
-            count = 0
-            return alert ("por favor, preencha todos os campos")
-        }else{
-            login(loginBody)
-        }
-    })
+    inputs.forEach((input) => {
+      if (input.value.trim() === "") {
+        count++;
+      }
+      loginBody[input.name] = input.value;
+    });
+    if (count != 0) {
+      count = 0;
+      toasts("por favor, preencha todos os campos", red);
+    } else {
+      login(loginBody);
+    }
+  });
 }
 
 const buttonCadastre = () => {
-    const button = document.querySelector(".redirect-edit__button")
-    
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        location.replace("./src/pages/signup.html");
-    })
-}
-handleLogin()
+  const button = document.querySelector(".redirect-edit__button");
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    location.replace("./src/pages/signup.html");
+  });
+};
+handleLogin();
 buttonCadastre();
